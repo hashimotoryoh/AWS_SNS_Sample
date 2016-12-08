d5d505f801b68c4a18a2022b79ed415223af602d
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2016-12-08 19:10:54 +0900

    objective-cのsdkとのbridging headerを追加

---
+++ AWS_SNS_Sample/AWS_SDK_Bridging_Header.h
@@ -0,0 +1,14 @@
+//
+//  AWS_SDK_Bridging_Header.h
+//  AWS_SNS_Sample
+//
+//  Created by Ryoh Hashimoto on 2016/12/08.
+//  Copyright © 2016年 Ryoh Hashimoto. All rights reserved.
+//
+
+#ifndef AWS_SDK_Bridging_Header_h
+#define AWS_SDK_Bridging_Header_h
+
+#import <AWSSNS/AWSSNS.h>
+
+#endif /* AWS_SDK_Bridging_Header_h */
