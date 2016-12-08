9517355dd94f64f370dc5b2acb1ee215760c8b3e
Ryoh Hashimoto <hashimotoryoh@gmail.com>
2016-12-08 18:30:00 +0900

    create new swift proj

---
+++ AWS_SNS_Sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWS_SNS_Sample
+//
+//  Created by Ryoh Hashimoto on 2016/12/08.
+//  Copyright © 2016年 Ryoh Hashimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
